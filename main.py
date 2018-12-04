4734827b186a1d56f6e1e3a21ca69c416a5f8f1b
Davis Peixoto <davis.peixoto@gmail.com>
2018-12-04 10:53:38 -0200

    dependencies sklearn and xgboost. created the main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from xgboost import XGBRegressor
+
+
